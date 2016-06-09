0625783bcec8a4d9c6a8c106bf6b95426bca3715
Tianxin Li <Tianxin Li>
2016-06-09 19:26:35 -0400

    added function: do not create multiple tab, bring opened tab to front

--- background.js
+++ background.js
@@ -1,7 +1,17 @@
 /**
  * Creates a outlook window when the badge is clicked
  */
+
+ var windowId = null;
+
  chrome.browserAction.onClicked.addListener(function() {
-    chrome.windows.create({'url': 'https://mail.live.com/m/', 'type': 'popup','width': 850, 'height': 800}, function(window) {
+   // check if the app is already opened
+   if(!windowId) {
+    chrome.windows.create({'url': 'https://mail.live.com/m/', 'type': 'popup','width': 950, 'height': 800}, function(window) {
+      windowId = window.id;
     });
+  } else {
+    chrome.windows.update(windowId, {'focused': true} , function(window) {});
+
+  }
  });
