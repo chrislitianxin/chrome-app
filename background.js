85bf8f12f0abc7b2210e102639e4a36ad690f3e2
Li Tianxin <chrislitianxin@gmail.com>
2017-07-05 11:07:33 -0400

    Update background.js

--- background.js
+++ background.js
@@ -7,7 +7,7 @@
  chrome.browserAction.onClicked.addListener(function() {
    // check if the app is already opened
    if(!windowId) {
-    chrome.windows.create({'url': 'https://mail.live.com/m/', 'type': 'popup','width': 950, 'height': 800}, function(window) {
+    chrome.windows.create({'url': 'https://outlook.live.com/owa/', 'type': 'popup','width': 950, 'height': 800}, function(window) {
       windowId = window.id;
     });
   } else {
